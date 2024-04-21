e2a0551bd744cdb2c5ef3651f01c1853158b85a0
Richard Copra <rickcopra@gmail.com>
2024-04-21 13:27:00 +0200

    wrote psuedo code to do list in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,9 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+// TODO:
+// add event listener to search button
+// when button is clicked, we take value from input field and create API URL
+// fetch select data from the API, and filter relevant data (name, address, rating, cuisines)
+// update index.html via DOM, limiting results to 10 restaurants
